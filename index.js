6d0c7e341a2aff5d2fbfe2191980e285127b2c41
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-11-13 15:58:02 +0100

    refactor: partially modified the code to move from imperative programming to functional programming

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // Import Modules
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
 const fs = require("fs");
 
 // Declaration of data structures
@@ -12,43 +12,23 @@ const pointLetters = {'A': 1, 'B': 5, 'C': 2, 'D': 5, 'E': 1, 'F': 5, 'G': 8, 'H
     'I': 1, 'L': 3, 'M': 3, 'N': 3, 'O': 1, 'P': 5, 'Q': 10, 'R': 2,
     'S': 2, 'T': 2, 'U': 3, 'V': 5, 'Z': 8 };
 
-let available_letters = [];
-
 // Creation of the object containing all the available letters
-for (let i of alphabet) {
-    for (let quantity = 0; quantity < Object.values(i); quantity++){
-        available_letters.push(Object.keys(i))}
-}
+let available_letters = createAvailableLetters(alphabet);
 
 // console.log(available_letters)  // debug
 // console.log(available_letters.length)  // debug
 
-
 // let hand = ["A", "B", "B", "A", "G", "L", "I", "A"]  // debug
-let hand = [];
 
-for (let i = 1; i<=8; i++) {
-    let tmp = available_letters[Math.floor(Math.random() * available_letters.length)]
-    hand.push(available_letters.splice(available_letters.indexOf(tmp), 1)[0][0]);
-}
-let handScore = calculateScore(hand)
-console.log(handScore)
+let hand = createHand(available_letters)
+let handScoreMax = calculateScore(hand)
 
 console.log(hand)   // debug
+console.log(handScoreMax) // debug
+
 // console.log(available_letters.length)  // debug
 
-
 // let word = prompt('Enter a word: ');
-
-
-function calculateScore(word) {
-    let points = 0;
-    for (let i = 0; i < word.length; i++) {
-        let character = word[i];
-        points += pointLetters[character.toUpperCase()];
-    }
-    return points;
-}
 
 // File reading
 fs.readFile('./it_words_allowed_scrabble.txt', 'utf8',
@@ -59,7 +39,7 @@ fs.readFile('./it_words_allowed_scrabble.txt', 'utf8',
         }
         // Successful file reading
         const array = data.split("\n");
-        // console.log(array)
+        // console.log(array)  // debug
 
         let result = ["", 0]
         let i = 0
@@ -76,13 +56,13 @@ fs.readFile('./it_words_allowed_scrabble.txt', 'utf8',
                     // console.log(item)
                     let score = calculateScore(item);
                     let score2 = result[1]
-                    if (score === handScore) {
+                    if (score === handScoreMax) { // for future -> >length is better ???
                         result[0] = item
                         result[1] = score
                         console.log(item)
                         console.log(score)
                         break;
-                    } else if (score > score2 && score <= handScore){
+                    } else if (score > score2 && score <= handScoreMax){
                         result[0] = item;
                         result[1] = score;
                     }
@@ -93,3 +73,36 @@ fs.readFile('./it_words_allowed_scrabble.txt', 'utf8',
         console.log(result[0])
         console.log(result[1])
 });
+
+
+// FUNCTIONS ==============================================================================
+
+function createAvailableLetters(alphabet) {
+    let available_letters = [];
+    for (let i of alphabet) {
+        for (let quantity = 0; quantity < Object.values(i); quantity++) {
+            available_letters.push(Object.keys(i))
+        }
+    }
+    return available_letters
+}
+
+function createHand(available_letters) {
+    let hand = [];
+    for (let i = 1; i <= 8; i++) {
+        let tmp = available_letters[Math.floor(Math.random() * available_letters.length)]
+        hand.push(available_letters.splice(available_letters.indexOf(tmp), 1)[0][0]);
+    }
+    return hand
+}
+
+function calculateScore(word) {
+    let points = 0;
+    for (let i = 0; i < word.length; i++) {
+        let character = word[i];
+        points += pointLetters[character.toUpperCase()];
+    }
+    return points;
+}
+
+// ==========================================================================================
